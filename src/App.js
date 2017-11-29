60742874114f0b3a209fbd92c17692f15ebb02c9
Caterina <caterinadasilva@gmail.com>
2017-11-29 18:28:12 -0300

    Register master ready

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import logo from '../src/static/img/pokemon_logo.png';
 class Welcome extends Component {
   render() {
     return (
-      <div className="title">
-      <img src={logo} alt="Pokémon Battle"/>
-        <h1>Battle!</h1>
+    	<div className="welcome">
+	      <div className="title">
+	      	<img src={logo} alt="Pokémon Battle"/>
+	        <h1>Battle!</h1>
+	      </div>
+	      <div className="login">
+	      	<h3>Hello trainer!</h3>
+	      	<div className="form-group">
+				    <label htmlFor="trainerName">What is your name?</label>
+				    <input type="text" className="form-control" id="trainerName"/>
+				  </div>
+	      	<button className="btn btn-lg btn-danger">Start!</button>
+	      </div>
       </div>
     );
   }
