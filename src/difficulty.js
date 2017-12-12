3f2da134ca43a99e53b3f255460612741e8dd07b
Vann Pellegrini <vann@vann.local>
2017-12-12 16:07:38 -0300

    [WIP]

---
+++ src/difficulty.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Difficulty extends Component {
+	render() {
+		return{
+			
+		}
+	}
+}
+export default Difficulty;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Selection from './selection';
+import Difficulty from './difficulty';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
